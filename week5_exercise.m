b8585443ec3eec77466a4570f715bd59e57e9130
eas2655-taka <35376793+eas2655-taka@users.noreply.github.com>
2018-10-02 13:16:57 -0400

    Add files via upload

--- week5_exercise.m
+++ week5_exercise.m
@@ -41,13 +41,47 @@ for k = K:-1:2
     Z(:,:,k-1)=Z(:,:,k)+alpha(:,:,k)/g*(depth(k)-depth(k-1))*1e4;
 end
 
+% plotting dynamic height
 figure(2);
 m_proj('robinson','clon',180);
-m_pcolor(lon,lat,Z(:,:,1)');
+m_pcolor(lon,lat,Z(:,:,1)'); % color shading
+hold on;
+m_contour(lon,lat,Z(:,:,1)',[-1:0.1:3],'k-'); % add contour lines
+hold off;
 shading flat;
 colormap('jet');
 caxis([-1 3]);
 m_grid;
+m_coast;
+colorbar('hori');
+title('surface dynamic height based on WOA2009');
+
+% plotting specific volume
+figure(3);
+m_proj('robinson','clon',180);
+m_pcolor(lon,lat,alpha(:,:,1)'); % color shading
+hold on;
+%m_contour(lon,lat,Z(:,:,1)',[-1:0.1:3],'k-'); % add contour lines
+hold off;
+shading flat;
+colormap('jet');
+caxis([-1 1]*1e-5);
+m_grid;
+m_coast;
+colorbar('hori');
+title('surface specific volume anomaly based on WOA2009');
+
+% plotting in polar projection
+figure(4);
+m_proj('stereographic','lat',-90,'radius',60); % polar projection
+m_pcolor(lon,lat,Z(:,:,1)'); % color shading
+hold on;
+m_contour(lon,lat,Z(:,:,1)',[-1:0.1:3],'k-'); % add contour lines
+hold off;
+shading flat;
+colormap('jet');
+caxis([-1 3]);
+m_grid('xaxisloc','top');
 m_coast;
 colorbar('hori');
 title('surface dynamic height based on WOA2009');
